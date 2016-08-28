568e83dc22e6fca7cbed942ddb3717e8a25288a6
Xrgerx01 <xrgerx@xrgerxs-imac.local>
2016-08-27 21:34:56 -0700

    ninguno

--- TareaPeticion/ViewController.swift
+++ TareaPeticion/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate {
 
+    @IBOutlet weak var resultado: UITextView!
+    @IBOutlet weak var isbn: UITextField!
+    var textoresultado: String = ""
+    var urlstable: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        isbn.delegate = self
+        isbn.keyboardType = UIKeyboardType.WebSearch
+        resultado.textColor = UIColor.blueColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField){
+       sender.resignFirstResponder()
     }
-
-
+    
+    func sincrono() {
+        let urls = String(urlstable) + isbn.text!
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        if datos == nil {
+         let alerta = UIAlertController(title: "Sin Conexion", message: "No se pudo conectar con el Servidor", preferredStyle: .Alert)
+         let accionError = UIAlertAction(title: "Error", style: .Default, handler: {
+          accion in //..
+          })
+         alerta.addAction(accionError)
+         self.presentViewController(alerta, animated: true, completion: nil)
+        }
+        else{
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            self.textoresultado = String(texto!)
+            if self.textoresultado == "{}" {
+                self.textoresultado = "EL ISBN NO HA SIDO ENCONTRADO"
+            }
+            
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        isbn.text = textField.text!
+        sincrono()
+        resultado.text = textoresultado
+        return true
+    }
+    
+    @IBAction func textFieldDidBeginEditing(textField: UITextField){
+      //textField.resignFirstResponder()
+    }
+    
+    @IBAction func textFieldDidEndEditing(textField: UITextField) {
+      //textField.resignFirstResponder()
+    }
+    
+    @IBAction func limpiar() {
+        isbn.text = ""
+        resultado.text = ""
+    }
 }
 
